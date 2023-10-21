174e820943604dfa67d33227290bc798758141dc
str4ng3love <normalemailadress@gmail.com>
2023-10-21 17:15:01 +0200

    delete test

--- src/app/ui/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import Spinerini from "@/app/components/Spinerini" 
-
-const loading = () => {
-  return (
-    <div className="min-h-[calc(100dvh_-_10rem)] -translate-x-[6.5%] flex items-center justify-center w-full flex-col"><Spinerini borderSize="border-[1rem]" h="h-32" w="w-32"/><h1 className="p-8 mt-20 font-bold text-5xl">Loading...</h1></div>
-  )
-}
-
-export default loading
